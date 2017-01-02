b515f749024d348d4f8ae9316374e20b0daf1079
Bernhard Stoecker <bernhard.stoecker@recogizer.de>
2017-01-02 09:54:17 +0100

    Fix docker setup

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -16,16 +16,14 @@ object WebServer {
     val route =
       path("hello") {
         get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+          controllers.HelloController.getHello()
         }
       }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+    while(true) {
+      Thread.sleep(1000)
+    }
   }
 }
